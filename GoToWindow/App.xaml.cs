3439a6a26b48befd7443b2cfff368e21894f3cdf
nwz <nwz88348307@163.com>
2018-12-26 16:01:42 +0800

    添加安装功能，添加判断二次启动

--- GoToWindow/App.xaml.cs
+++ GoToWindow/App.xaml.cs
@@ -13,167 +13,171 @@ using GoToWindow.ViewModels;
 
 namespace GoToWindow
 {
-	public partial class App
-	{
-		private static readonly ILog Log = LogManager.GetLogger(typeof(App).Assembly, "GoToWindow");
+    public partial class App
+    {
+        private static readonly ILog Log = LogManager.GetLogger(typeof(App).Assembly, "GoToWindow");
 
-		private IGoToWindowContext _context;
-		private GoToWindowTrayIcon _menu;
-		private Mutex _mutex;
+        private IGoToWindowContext _context;
+        private GoToWindowTrayIcon _menu;
+        private Mutex _mutex;
 
-		private void Application_Startup(object sender, StartupEventArgs e)
-		{
-			var isFirstRun = false;
-			if(e.Args.Any() && e.Args[0].StartsWith("--squirrel"))
-			{
-				var cliHandler = new SquirrelCommandLineArgumentsHandler();
-				if (cliHandler.HandleSquirrelArguments(e.Args))
-				{
-					Log.Info("Handled Squirrel arguments. Shutting down.");
-					Current.Shutdown(1);
-					return;
-				}
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            var isFirstRun = false;
+            if (e.Args.Any() && e.Args[0].StartsWith("--squirrel"))
+            {
+                var cliHandler = new SquirrelCommandLineArgumentsHandler();
+                if (cliHandler.HandleSquirrelArguments(e.Args))
+                {
+                    Log.Info("Handled Squirrel arguments. Shutting down.");
+                    Current.Shutdown(1);
+                    return;
+                }
 
-				isFirstRun = cliHandler.IsFirstRun;
-			}
+                isFirstRun = cliHandler.IsFirstRun;
+            }
 
-			if (!WaitForOtherInstancesToShutDown())
-			{
-				MessageBox.Show(
-					"Another Go To Window instance is already running." + Environment.NewLine +
-					"Exit by right-clicking the icon in the tray, and selecting 'Exit'.",
-					"Go To Window",
-					MessageBoxButton.OK,
-					MessageBoxImage.Information
-					);
-				Log.Warn("Application already running. Shutting down.");
-				Current.Shutdown(1);
-				return;
-			}
+            if (!WaitForOtherInstancesToShutDown())
+            {
+                MessageBox.Show(
+                    "Another Go To Window instance is already running." + Environment.NewLine +
+                    "Exit by right-clicking the icon in the tray, and selecting 'Exit'.",
+                    "Go To Window",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Information
+                    );
+                Log.Warn("Application already running. Shutting down.");
+                Current.Shutdown(1);
+                return;
+            }
 
-			// http://stackoverflow.com/questions/14635862/exception-occurs-while-pressing-a-button-on-touchscreen-using-a-stylus-or-a-fing
-			DisableWpfTabletSupport();
+            // http://stackoverflow.com/questions/14635862/exception-occurs-while-pressing-a-button-on-touchscreen-using-a-stylus-or-a-fing
+            DisableWpfTabletSupport();
 
-			_context = new GoToWindowContext();
-			
-			_menu = new GoToWindowTrayIcon(_context);
+            _context = new GoToWindowContext();
 
-			var shortcut = KeyboardShortcut.FromString(GoToWindow.Properties.Settings.Default.OpenShortcut);
-			_context.EnableKeyboardHook(shortcut);
+            _menu = new GoToWindowTrayIcon(_context);
 
-			_context.Init();
+            var shortcut = KeyboardShortcut.FromString(GoToWindow.Properties.Settings.Default.OpenShortcut);
+            _context.EnableKeyboardHook(shortcut);
 
-			if (shortcut.IsValid)
-				Log.InfoFormat("Application started. Shortcut is '{0}' ({1})", shortcut.ToHumanReadableString(), GoToWindow.Properties.Settings.Default.OpenShortcut);
-			else
-				Log.WarnFormat("Application started with invalid shortcut. Shortcut is '{0}', reason: {1}", GoToWindow.Properties.Settings.Default.OpenShortcut, shortcut.InvalidReason);
+            _context.Init();
 
-			if (isFirstRun)
-			{
-				Log.Info("Squirrel: First run");
-				Dispatcher.InvokeAsync(() =>
-				{
-					new FirstRunWindow(_context) {DataContext = new FirstRunViewModel()}.Show();
-				});
-			}
-			else
-			{
+            if (shortcut.IsValid)
+                Log.InfoFormat("Application started. Shortcut is '{0}' ({1})", shortcut.ToHumanReadableString(), GoToWindow.Properties.Settings.Default.OpenShortcut);
+            else
+                Log.WarnFormat("Application started with invalid shortcut. Shortcut is '{0}', reason: {1}", GoToWindow.Properties.Settings.Default.OpenShortcut, shortcut.InvalidReason);
+
+            if (isFirstRun)
+            {
+                Log.Info("Squirrel: First run");
+                Dispatcher.InvokeAsync(() =>
+                {
+                    new FirstRunWindow(_context) { DataContext = new FirstRunViewModel() }.Show();
+                });
+            }
+            else
+            {
                 if (GoToWindow.Properties.Settings.Default.IsShowMenuTip)
                 {
                     GoToWindow.Properties.Settings.Default.IsShowMenuTip = false;
                     GoToWindow.Properties.Settings.Default.Save();
                     _menu.ShowStartupTooltip();
                 }
-			}
+            }
 
-			SquirrelContext.AcquireUpdater().CheckForUpdates(_context.UpdateAvailable, null);
-		}
+            SquirrelContext.AcquireUpdater().CheckForUpdates(_context.UpdateAvailable, null);
+        }
 
-		private bool WaitForOtherInstancesToShutDown()
-		{
-			const int msBetweenAttempts = 500;
-			for(var attempt = 0; attempt < 10; attempt++)
-			{
-				_mutex = new Mutex(true, "GoToWindow", out bool isOnlyRunningProcessInstance);
+        private bool WaitForOtherInstancesToShutDown()
+        {
+            //const int msBetweenAttempts = 500;
+            //for(var attempt = 0; attempt < 10; attempt++)
+            //{
+            //	_mutex = new Mutex(true, "GoToWindow", out bool isOnlyRunningProcessInstance);
 
-				if (isOnlyRunningProcessInstance)
-					return true;
+            //	if (isOnlyRunningProcessInstance)
+            //		return true;
 
-				Log.DebugFormat("Another instance is running. Waiting for {0}ms (attempt {1})...", msBetweenAttempts, attempt + 1);
-				Thread.Sleep(msBetweenAttempts);
-			}
+            //	Log.DebugFormat("Another instance is running. Waiting for {0}ms (attempt {1})...", msBetweenAttempts, attempt + 1);
+            //	Thread.Sleep(msBetweenAttempts);
+            //}
+            _mutex = new Mutex(true, "GoToWindow", out bool isOnlyRunningProcessInstance);
 
-			return false;
-		}
+            if (isOnlyRunningProcessInstance)
+                return true;
 
-		private void Application_Exit(object sender, ExitEventArgs e)
-		{
+            return false;
+        }
+
+        private void Application_Exit(object sender, ExitEventArgs e)
+        {
             SquirrelContext.Dispose();
 
-			if (_menu != null)
-			{
-				_menu.Dispose();
-				_menu = null;
-			}
+            if (_menu != null)
+            {
+                _menu.Dispose();
+                _menu = null;
+            }
 
-			if (_context != null)
-			{
-				_context.Dispose();
-				_context = null;
-			}
+            if (_context != null)
+            {
+                _context.Dispose();
+                _context = null;
+            }
 
-			if (_mutex != null)
-			{
-				_mutex.Dispose();
-				_mutex = null;
-			}
+            if (_mutex != null)
+            {
+                _mutex.Dispose();
+                _mutex = null;
+            }
 
-			Log.Info("Application exited.");
-		}
+            Log.Info("Application exited.");
+        }
 
-		private void Application_Activated(object sender, EventArgs e)
-		{
-			Log.Debug("Application activated.");
-		}
+        private void Application_Activated(object sender, EventArgs e)
+        {
+            Log.Debug("Application activated.");
+        }
 
-		private void Application_Deactivated(object sender, EventArgs e)
-		{
-			Log.Debug("Application deactivated.");
+        private void Application_Deactivated(object sender, EventArgs e)
+        {
+            Log.Debug("Application deactivated.");
 
-			_context?.Hide(false);
-		}
+            _context?.Hide(false);
+        }
 
-		private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
-		{
-			Log.Fatal(e.Exception);
-			MessageBox.Show("An error occured. Go To Window will shut down. Error details are available in GoToWindow.log.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
-			e.Handled = true;
-			Current.Shutdown((int)ExitCodes.UnhandledError);
-		}
+        private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            Log.Fatal(e.Exception);
+            MessageBox.Show("An error occured. Go To Window will shut down. Error details are available in GoToWindow.log.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            e.Handled = true;
+            Current.Shutdown((int)ExitCodes.UnhandledError);
+        }
 
-		public static void DisableWpfTabletSupport()
-		{
-			var devices = Tablet.TabletDevices;
+        public static void DisableWpfTabletSupport()
+        {
+            var devices = Tablet.TabletDevices;
 
-			if (devices.Count <= 0) return;
-			
-			var inputManagerType = typeof(InputManager);
+            if (devices.Count <= 0) return;
 
-			// Call the StylusLogic method on the InputManager.Current instance.
-			var stylusLogic = inputManagerType.InvokeMember("StylusLogic",
-				BindingFlags.GetProperty | BindingFlags.Instance | BindingFlags.NonPublic,
-				null, InputManager.Current, null);
+            var inputManagerType = typeof(InputManager);
 
-			if (stylusLogic == null) return;
+            // Call the StylusLogic method on the InputManager.Current instance.
+            var stylusLogic = inputManagerType.InvokeMember("StylusLogic",
+                BindingFlags.GetProperty | BindingFlags.Instance | BindingFlags.NonPublic,
+                null, InputManager.Current, null);
 
-			var stylusLogicType = stylusLogic.GetType();
+            if (stylusLogic == null) return;
 
-			while (devices.Count > 0)
-			{
-				stylusLogicType.InvokeMember("OnTabletRemoved",
-					BindingFlags.InvokeMethod | BindingFlags.Instance | BindingFlags.NonPublic,
-					null, stylusLogic, new object[] {(uint) 0});
-			}
-		}
-	}
+            var stylusLogicType = stylusLogic.GetType();
+
+            while (devices.Count > 0)
+            {
+                stylusLogicType.InvokeMember("OnTabletRemoved",
+                    BindingFlags.InvokeMethod | BindingFlags.Instance | BindingFlags.NonPublic,
+                    null, stylusLogic, new object[] { (uint)0 });
+            }
+        }
+    }
 }
--- GotoWindow2/App.xaml.cs
+++ GotoWindow2/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -17,8 +18,21 @@ namespace GotoWindow2
     {
         private KeyboardHookAlt _hook;
         MainWindow mMainWin;
+        private Mutex _mutex;
+
         private void Application_Startup(object sender, StartupEventArgs e)
         {
+            if (!WaitForOtherInstancesToShutDown())
+            {
+                MessageBox.Show(
+                    "Another Go To Window instance is already running.",
+                    "Go To Window",
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Information
+                    );
+                Current.Shutdown(1);
+                return;
+            }
             _hook = KeyboardHookAlt.Hook(KeyHookCallback);
             mMainWin = new MainWindow();
         }
@@ -30,7 +44,31 @@ namespace GotoWindow2
 
         private void Application_Exit(object sender, ExitEventArgs e)
         {
-            _hook.Dispose();
+            if (_hook != null)
+            {
+                _hook.Dispose();
+            }
+            if (_mutex != null)
+            {
+                _mutex.Dispose();
+                _mutex = null;
+            }
+        }
+
+        private bool WaitForOtherInstancesToShutDown()
+        {
+            const int msBetweenAttempts = 500;
+            for (var attempt = 0; attempt < 10; attempt++)
+            {
+                _mutex = new Mutex(true, "GoToWindow2", out bool isOnlyRunningProcessInstance);
+
+                if (isOnlyRunningProcessInstance)
+                    return true;
+
+                Thread.Sleep(msBetweenAttempts);
+            }
+
+            return false;
         }
     }
 }
